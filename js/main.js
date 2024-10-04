e891d498bff8cada4232d71644db4115e96b5400
DIMIBAGA <dianabarrantesga1510@gmail.com>
2024-10-04 14:25:42 -0500

    commit

--- js/main.js
+++ js/main.js
@@ -163,4 +163,24 @@ function filterSpeakers() {
         }
     });
 }
+
+function actualizarContador() {
+  const fechaObjetivo = new Date('October 26, 2024 00:00:00').getTime();
+  const ahora = new Date().getTime();
+  const diferencia = fechaObjetivo - ahora;
+
+  const dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+  const horas = Math.floor((diferencia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutos = Math.floor((diferencia % (1000 * 60 * 60)) / (1000 * 60));
+  const segundos = Math.floor((diferencia % (1000 * 60)) / 1000);
+
+  document.getElementById('dias').innerHTML = dias;
+  document.getElementById('horas').innerHTML = horas;
+  document.getElementById('minutos').innerHTML = minutos;
+  document.getElementById('segundos').innerHTML = segundos;
+}
+
+actualizarContador();
+setInterval(actualizarContador, 1000);
+
 })(jQuery);
